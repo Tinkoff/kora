5d0df2261023453495a3e5649e4c3c702174926e
Anton Kurako <goodforgod.dev@gmail.com>
2022-11-03 10:50:48 +0300

    [validation-runtime)] ValidationSymbolProcessor proper string processing ValidationContext path creation and context creation added ValidationContext.Builder added

--- validation/validation-common/src/main/java/ru/tinkoff/kora/validation/ValidationContext.java
+++ validation/validation-common/src/main/java/ru/tinkoff/kora/validation/ValidationContext.java
@@ -10,6 +10,18 @@ public interface ValidationContext {
 
         Path root();
 
+        default Path add(@NotNull String field) {
+            return new SimpleValidationContext.SimpleFieldPath(this, field);
+        }
+
+        default Path add(int index) {
+            return new SimpleValidationContext.SimpleIndexPath(this, index);
+        }
+
+        static Path of(@NotNull String path) {
+            return new SimpleValidationContext.SimpleFieldPath(null, path);
+        }
+
         @NotNull
         default String full() {
             return toString();
@@ -22,15 +34,31 @@ public interface ValidationContext {
     boolean isFailFast();
 
     default ValidationContext addPath(@NotNull String path) {
-        return new SimpleValidationContext(new SimpleValidationContext.SimpleFieldPath(path(), path), isFailFast());
+        return new SimpleValidationContext(path().add(path), isFailFast());
     }
 
     default ValidationContext addPath(int pathIndex) {
-        return new SimpleValidationContext(new SimpleValidationContext.SimpleIndexPath(path(), pathIndex), isFailFast());
+        return new SimpleValidationContext(path().add(pathIndex), isFailFast());
     }
 
     @NotNull
     default Violation violates(@NotNull String message) {
         return new SimpleViolation(message, path());
+    }
+
+    static Builder builder() {
+        return new SimpleValidationContext.SimpleBuilder(SimpleValidationContext.SimpleFieldPath.ROOT, false);
+    }
+
+    interface Builder {
+
+        @NotNull
+        Builder path(@NotNull Path path);
+
+        @NotNull
+        Builder failFast(boolean isFailFast);
+
+        @NotNull
+        ValidationContext build();
     }
 }
