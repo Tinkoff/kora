66b71b911da47efce1f1088e5407f2e279403bd9
Anton Kurako <goodforgod.dev@gmail.com>
2022-12-27 16:14:55 +0300

    Tests fixed

--- test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/NonComponentJUnitExtensionTests.java
+++ test/test-junit5/src/test/java/ru/tinkoff/kora/test/extension/junit5/NonComponentJUnitExtensionTests.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import ru.tinkoff.kora.config.annotation.processor.processor.ConfigRootAnnotationProcessor;
 import ru.tinkoff.kora.config.annotation.processor.processor.ConfigSourceAnnotationProcessor;
 import ru.tinkoff.kora.http.client.annotation.processor.HttpClientAnnotationProcessor;
+import ru.tinkoff.kora.kora.app.annotation.processor.KoraAppProcessor;
 
 @KoraAppTest(application = HttpClientApplication.class,
     components = {HttpClientImpl.class},
     processors = {
+        KoraAppProcessor.class,
         HttpClientAnnotationProcessor.class,
         ConfigRootAnnotationProcessor.class,
         ConfigSourceAnnotationProcessor.class},
