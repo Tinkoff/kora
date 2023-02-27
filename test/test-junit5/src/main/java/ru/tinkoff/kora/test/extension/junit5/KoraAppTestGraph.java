e02dfba875fedd5c8942d2afc8af4716815fc662
Anton Kurako <goodforgod.dev@gmail.com>
2023-02-27 12:58:47 +0300

    Javadoc added

--- test/test-junit5/src/main/java/ru/tinkoff/kora/test/extension/junit5/KoraAppTestGraph.java
+++ test/test-junit5/src/main/java/ru/tinkoff/kora/test/extension/junit5/KoraAppTestGraph.java
@@ -6,9 +6,24 @@ import javax.annotation.Nullable;
 
 public interface KoraAppTestGraph {
 
+    /**
+     * Try to find implementation in Graph by type
+     *
+     * @param type to look for
+     * @return type instance from Graph
+     * @param <T> type parameter
+     */
     @Nullable
     <T> T get(@NotNull Class<T> type);
 
+    /**
+     * Try to find implementation in Graph by type and tags
+     *
+     * @param type to look for
+     * @param tags associated with type
+     * @return type instance from Graph
+     * @param <T> type parameter
+     */
     @Nullable
     <T> T get(@NotNull Class<T> type, Class<?>... tags);
 }
