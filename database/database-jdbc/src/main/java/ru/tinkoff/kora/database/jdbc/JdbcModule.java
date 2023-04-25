0ce2346b0dc3a29a8642d4a596ec862265c6ff6a
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2023-04-25 18:27:29 +0300

    java.time, uuid and BigDecimal support for r2dbc and jdbc (#125)
    * JdbcModule & R2dbcModule mappers improved
    
    * JdbcModule & R2dbcModule BigDecimal support added
    New mappers marked as @DefaultComponent
    
    * JdbcModule UUID row mapper added

--- database/database-jdbc/src/main/java/ru/tinkoff/kora/database/jdbc/JdbcModule.java
+++ database/database-jdbc/src/main/java/ru/tinkoff/kora/database/jdbc/JdbcModule.java
@@ -1,15 +1,20 @@
 package ru.tinkoff.kora.database.jdbc;
 
+import ru.tinkoff.kora.common.DefaultComponent;
 import ru.tinkoff.kora.database.common.DataBaseModule;
+import ru.tinkoff.kora.database.jdbc.mapper.parameter.JdbcParameterColumnMapper;
+import ru.tinkoff.kora.database.jdbc.mapper.result.JdbcResultColumnMapper;
 import ru.tinkoff.kora.database.jdbc.mapper.result.JdbcResultSetMapper;
 import ru.tinkoff.kora.database.jdbc.mapper.result.JdbcRowMapper;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.sql.Types;
+import java.time.*;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface JdbcModule extends DataBaseModule {
+
     default <T> JdbcResultSetMapper<Optional<T>> optionalResultSetMapper(JdbcRowMapper<T> rowMapper) {
         return JdbcResultSetMapper.optionalResultSetMapper(rowMapper);
     }
@@ -64,16 +69,6 @@ public interface JdbcModule extends DataBaseModule {
         };
     }
 
-    default JdbcRowMapper<BigDecimal> bigDecimalJdbcRowMapper() {
-        return rs -> {
-            var value = rs.getObject(1, BigDecimal.class);
-            if (rs.wasNull()) {
-                return null;
-            }
-            return value;
-        };
-    }
-
     default JdbcRowMapper<byte[]> byteArrayJdbcRowMapper() {
         return rs -> {
             var value = rs.getBytes(1);
@@ -84,6 +79,51 @@ public interface JdbcModule extends DataBaseModule {
         };
     }
 
+    @DefaultComponent
+    default JdbcRowMapper<BigDecimal> bigDecimalJdbcRowMapper() {
+        return rs -> {
+            var value = rs.getObject(1, BigDecimal.class);
+            if (rs.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcRowMapper<UUID> uuidJdbcRowMapper() {
+        return rs -> {
+            var value = rs.getObject(1, UUID.class);
+            if (rs.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcRowMapper<LocalDate> localDateJdbcRowMapper() {
+        return rs -> {
+            var value = rs.getObject(1, LocalDate.class);
+            if (rs.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcRowMapper<LocalTime> localTimeJdbcRowMapper() {
+        return rs -> {
+            var value = rs.getObject(1, LocalTime.class);
+            if (rs.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
     default JdbcRowMapper<LocalDateTime> localDateTimeJdbcRowMapper() {
         return rs -> {
             var value = rs.getObject(1, LocalDateTime.class);
@@ -94,10 +134,178 @@ public interface JdbcModule extends DataBaseModule {
         };
     }
 
-    default JdbcRowMapper<LocalDate> localDateJdbcRowMapper() {
+    @DefaultComponent
+    default JdbcRowMapper<OffsetTime> offsetTimeJdbcRowMapper() {
         return rs -> {
-            var value = rs.getObject(1, LocalDate.class);
+            var value = rs.getObject(1, OffsetTime.class);
             if (rs.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcRowMapper<OffsetDateTime> offsetDateTimeJdbcRowMapper() {
+        return rs -> {
+            var value = rs.getObject(1, OffsetDateTime.class);
+            if (rs.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    // Parameter Mappers
+    @DefaultComponent
+    default JdbcParameterColumnMapper<BigDecimal> bigDecimalJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.NUMERIC);
+            } else {
+                stmt.setObject(index, o, Types.NUMERIC);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default JdbcParameterColumnMapper<UUID> uuidJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.OTHER);
+            } else {
+                stmt.setObject(index, o, Types.OTHER);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default JdbcParameterColumnMapper<LocalDate> localDateJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.DATE);
+            } else {
+                stmt.setObject(index, o, Types.DATE);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default JdbcParameterColumnMapper<LocalTime> localTimeJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.TIME);
+            } else {
+                stmt.setObject(index, o, Types.TIME);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default JdbcParameterColumnMapper<LocalDateTime> LocalDateTimeJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.TIMESTAMP);
+            } else {
+                stmt.setObject(index, o, Types.TIMESTAMP);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default JdbcParameterColumnMapper<OffsetTime> offsetTimeJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.TIME_WITH_TIMEZONE);
+            } else {
+                stmt.setObject(index, o, Types.TIME_WITH_TIMEZONE);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default JdbcParameterColumnMapper<OffsetDateTime> offsetDateTimeJdbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.setNull(index, Types.TIMESTAMP_WITH_TIMEZONE);
+            } else {
+                stmt.setObject(index, o, Types.TIMESTAMP_WITH_TIMEZONE);
+            }
+        };
+    }
+
+    // Result Mappers
+    @DefaultComponent
+    default JdbcResultColumnMapper<BigDecimal> bigDecimalJdbcResultColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, BigDecimal.class);
+            if (row.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcResultColumnMapper<UUID> uuidJdbcResultColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, UUID.class);
+            if (row.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcResultColumnMapper<LocalDate> localDateJdbcColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, LocalDate.class);
+            if (row.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcResultColumnMapper<LocalTime> localTimeJdbcColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, LocalTime.class);
+            if (row.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcResultColumnMapper<LocalDateTime> localDateTimeJdbcColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, LocalDateTime.class);
+            if (row.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcResultColumnMapper<OffsetTime> offsetTimeJdbcColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, OffsetTime.class);
+            if (row.wasNull()) {
+                return null;
+            }
+            return value;
+        };
+    }
+
+    @DefaultComponent
+    default JdbcResultColumnMapper<OffsetDateTime> offsetDateTimeJdbcColumnMapper() {
+        return (row, index) -> {
+            var value = row.getObject(index, OffsetDateTime.class);
+            if (row.wasNull()) {
                 return null;
             }
             return value;
--- database/database-r2dbc/src/main/java/ru/tinkoff/kora/database/r2dbc/R2dbcModule.java
+++ database/database-r2dbc/src/main/java/ru/tinkoff/kora/database/r2dbc/R2dbcModule.java
@@ -1,16 +1,22 @@
 package ru.tinkoff.kora.database.r2dbc;
 
+import io.r2dbc.spi.Row;
+import io.r2dbc.spi.Statement;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import ru.tinkoff.kora.common.DefaultComponent;
 import ru.tinkoff.kora.database.common.DataBaseModule;
+import ru.tinkoff.kora.database.r2dbc.mapper.parameter.R2dbcParameterColumnMapper;
+import ru.tinkoff.kora.database.r2dbc.mapper.result.R2dbcResultColumnMapper;
 import ru.tinkoff.kora.database.r2dbc.mapper.result.R2dbcResultFluxMapper;
 import ru.tinkoff.kora.database.r2dbc.mapper.result.R2dbcRowMapper;
 
+import javax.annotation.Nullable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 import java.util.List;
+import java.util.UUID;
 
 public interface R2dbcModule extends DataBaseModule {
 
@@ -42,23 +48,160 @@ public interface R2dbcModule extends DataBaseModule {
         return row -> row.get(0, Double.class);
     }
 
-    default R2dbcRowMapper<LocalDate> localDateR2dbcRowMapper() {
-        return row -> row.get(0, LocalDate.class);
-    }
-
-    default R2dbcRowMapper<LocalDateTime> localDateTimeR2dbcRowMapper() {
-        return row -> row.get(0, LocalDateTime.class);
-    }
-
     default R2dbcRowMapper<BigDecimal> bigDecimalR2dbcRowMapper() {
         return row -> row.get(0, BigDecimal.class);
     }
 
+    @DefaultComponent
     default R2dbcRowMapper<BigInteger> bigIntegerR2dbcRowMapper() {
         return row -> row.get(0, BigInteger.class);
     }
 
     default R2dbcRowMapper<byte[]> byteArrayR2dbcRowMapper() {
         return row -> row.get(0, byte[].class);
+    }
+
+    @DefaultComponent
+    default R2dbcRowMapper<UUID> uuidR2dbcRowMapper() {
+        return row -> row.get(0, UUID.class);
+    }
+
+    @DefaultComponent
+    default R2dbcRowMapper<LocalDate> localDateR2dbcRowMapper() {
+        return row -> row.get(0, LocalDate.class);
+    }
+
+    @DefaultComponent
+    default R2dbcRowMapper<LocalTime> localTimeR2dbcRowMapper() {
+        return row -> row.get(0, LocalTime.class);
+    }
+
+    @DefaultComponent
+    default R2dbcRowMapper<LocalDateTime> localDateTimeR2dbcRowMapper() {
+        return row -> row.get(0, LocalDateTime.class);
+    }
+
+    @DefaultComponent
+    default R2dbcRowMapper<OffsetTime> offsetTimeR2dbcRowMapper() {
+        return row -> row.get(0, OffsetTime.class);
+    }
+
+    @DefaultComponent
+    default R2dbcRowMapper<OffsetDateTime> offsetDateTimeR2dbcRowMapper() {
+        return row -> row.get(0, OffsetDateTime.class);
+    }
+
+    // Parameter Mapper
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<BigDecimal> bigDecimalR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, BigDecimal.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<UUID> uuidR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, UUID.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<LocalDate> localDateR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, LocalDate.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<LocalTime> LocalTimeR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, LocalTime.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<LocalDateTime> LocalDateTimeR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, LocalDateTime.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<OffsetTime> offsetTimeR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, OffsetTime.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    @DefaultComponent
+    default R2dbcParameterColumnMapper<OffsetDateTime> OffsetDateTimeR2dbcParameterColumnMapper() {
+        return (stmt, index, o) -> {
+            if (o == null) {
+                stmt.bindNull(index, OffsetDateTime.class);
+            } else {
+                stmt.bind(index, o);
+            }
+        };
+    }
+
+    // Result Column Mappers
+    @DefaultComponent
+    default R2dbcResultColumnMapper<BigDecimal> bigDecimalR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, BigDecimal.class);
+    }
+
+    @DefaultComponent
+    default R2dbcResultColumnMapper<UUID> uuidR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, UUID.class);
+    }
+
+    @DefaultComponent
+    default R2dbcResultColumnMapper<LocalDate> localDateR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, LocalDate.class);
+    }
+
+    @DefaultComponent
+    default R2dbcResultColumnMapper<LocalTime> localTimeR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, LocalTime.class);
+    }
+
+    @DefaultComponent
+    default R2dbcResultColumnMapper<LocalDateTime> localDateTimeR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, LocalDateTime.class);
+    }
+
+    @DefaultComponent
+    default R2dbcResultColumnMapper<OffsetTime> offsetTimeR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, OffsetTime.class);
+    }
+
+    @DefaultComponent
+    default R2dbcResultColumnMapper<OffsetDateTime> offsetDateTimeR2dbcResultColumnMapper() {
+        return (row, label) -> row.get(label, OffsetDateTime.class);
     }
 }
