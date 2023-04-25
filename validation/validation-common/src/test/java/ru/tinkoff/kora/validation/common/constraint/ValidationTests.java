6bf207799b5fd337167bdc6bec9c15afe67efbcc
Anton Kurako <goodforgod.dev@gmail.com>
2023-04-25 17:48:25 +0700

    RangeLongNumberValidator case from double fixed ValidationTests reinforced

--- validation/validation-common/src/test/java/ru/tinkoff/kora/validation/common/constraint/ValidationTests.java
+++ validation/validation-common/src/test/java/ru/tinkoff/kora/validation/common/constraint/ValidationTests.java
@@ -8,6 +8,7 @@ import ru.tinkoff.kora.validation.common.Validator;
 import ru.tinkoff.kora.validation.common.annotation.Range;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -37,6 +38,32 @@ class ValidationTests extends Assertions implements ValidatorModule {
             Arguments.of(new RangeBigDecimalValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigDecimal.valueOf(0.5), 1),
             Arguments.of(new RangeBigDecimalValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigDecimal.valueOf(2), 0),
             Arguments.of(new RangeBigDecimalValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigDecimal.valueOf(1), 0),
+            Arguments.of(new RangeBigDecimalValidator(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), BigDecimal.valueOf(10), 0),
+            Arguments.of(new RangeBigDecimalValidator(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), BigDecimal.valueOf(-1), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), BigInteger.valueOf(3), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), BigInteger.valueOf(0), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), BigInteger.valueOf(2), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), BigInteger.valueOf(1), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), BigInteger.valueOf(2), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), BigInteger.valueOf(1), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigInteger.valueOf(3), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigInteger.valueOf(0), 1),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigInteger.valueOf(2), 0),
+            Arguments.of(new RangeBigIntegerValidator(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), BigInteger.valueOf(1), 0),
+            Arguments.of(new RangeBigIntegerValidator(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), BigInteger.valueOf(10), 0),
+            Arguments.of(new RangeBigIntegerValidator(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), BigInteger.valueOf(-1), 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 3, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 0, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 2, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 1, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 2, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 1, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 3, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 0, 1),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 2, 0),
+            Arguments.of(new RangeLongNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 1, 0),
+            Arguments.of(new RangeLongNumberValidator<>(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), 10, 0),
+            Arguments.of(new RangeLongNumberValidator<>(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), -1, 1),
             Arguments.of(new RangeDoubleNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 2.5, 1),
             Arguments.of(new RangeDoubleNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 0.5, 1),
             Arguments.of(new RangeDoubleNumberValidator<>(1, 2, Range.Boundary.EXCLUSIVE_EXCLUSIVE), 2, 1),
@@ -47,6 +74,8 @@ class ValidationTests extends Assertions implements ValidatorModule {
             Arguments.of(new RangeDoubleNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 0.5, 1),
             Arguments.of(new RangeDoubleNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 2, 0),
             Arguments.of(new RangeDoubleNumberValidator<>(1, 2, Range.Boundary.INCLUSIVE_INCLUSIVE), 1, 0),
+            Arguments.of(new RangeDoubleNumberValidator<>(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), 10, 0),
+            Arguments.of(new RangeDoubleNumberValidator<>(1, Long.MAX_VALUE, Range.Boundary.INCLUSIVE_INCLUSIVE), -1, 1),
             Arguments.of(new SizeMapValidator<>(2, 3), Map.of(1, 1, 2, 2), 0),
             Arguments.of(new SizeMapValidator<>(2, 3), Map.of(1, 1, 2, 2, 3, 3, 4, 4), 1),
             Arguments.of(new SizeMapValidator<>(2, 3), Map.of(1, 1), 1),
