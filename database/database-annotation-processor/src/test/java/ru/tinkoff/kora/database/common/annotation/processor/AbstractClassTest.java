e176d2f4f12bfc5bbaa1b81f9049d562f28de830
Anton Duyun <anton.duyun@gmail.com>
2023-06-16 15:56:14 +0300

    java fix

--- database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/AbstractClassTest.java
+++ database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/AbstractClassTest.java
@@ -2,21 +2,62 @@ package ru.tinkoff.kora.database.common.annotation.processor;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import ru.tinkoff.kora.database.common.annotation.processor.jdbc.MockJdbcExecutor;
-import ru.tinkoff.kora.database.common.annotation.processor.repository.AbstractClassRepository;
+import ru.tinkoff.kora.database.annotation.processor.RepositoryAnnotationProcessor;
+import ru.tinkoff.kora.database.common.annotation.processor.jdbc.AbstractJdbcRepositoryTest;
+import ru.tinkoff.kora.kora.app.annotation.processor.KoraAppProcessor;
 
 import java.sql.SQLException;
+import java.util.List;
 
-public class AbstractClassTest {
+public class AbstractClassTest extends AbstractJdbcRepositoryTest {
+    @Test
+    public void testAbstractClassRepository() throws SQLException {
+        var repository = compile("test", List.of(executor), """
+            @Repository
+            public abstract class TestRepository implements JdbcRepository {
+                private final String field;
+                        
+                public TestRepository(@Nullable String field) {
+                    this.field = field;
+                }
+                        
+                @Query("INSERT INTO table(value) VALUES (:value)")
+                public abstract void abstractMethod(String value);
+                        
+                public void nonAbstractMethod() {
+                        
+                }
+            }
+            """);
 
-    private final MockJdbcExecutor executor = new MockJdbcExecutor();
-    private final AbstractClassRepository repository = DbTestUtils.compile(AbstractClassRepository.class, "", executor);
+        repository.invoke("abstractMethod", "some-value");
 
+        Mockito.verify(executor.preparedStatement).setString(1, "some-value");
+    }
 
     @Test
-    void testNativeParameter() throws SQLException {
-        repository.abstractMethod("test");
+    public void testAbstractClassRepositoryExtension() throws SQLException {
+        compile(List.of(new RepositoryAnnotationProcessor(), new KoraAppProcessor()), """
+            @Repository
+            public abstract class TestRepository implements JdbcRepository {
+                @Query("INSERT INTO table(value) VALUES (:value)")
+                public abstract void abstractMethod(String value);
+                        
+                public void nonAbstractMethod() {
+                        
+                }
+            }
+            """, """
+            @KoraApp
+            public interface TestApp {
+                default ru.tinkoff.kora.database.jdbc.JdbcConnectionFactory factory() { return null; }
+                default String someString() { return null; }
+                
+                @Root
+                default Integer someRoot(TestRepository repository) { return 1; }
+            }
+            """);
+        compileResult.assertSuccess();
 
-        Mockito.verify(executor.preparedStatement).setString(1, "test");
     }
 }
