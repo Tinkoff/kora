3a269b750ee0fc6fdaf8156ce7b6cad7a877e3b3
Anton Kurako <goodforgod.dev@gmail.com>
2023-07-10 15:43:49 +0300

    RepositoryKoraExtension inheritance scan added

--- database/database-annotation-processor/src/main/java/ru/tinkoff/kora/database/annotation/processor/extension/RepositoryKoraExtension.java
+++ database/database-annotation-processor/src/main/java/ru/tinkoff/kora/database/annotation/processor/extension/RepositoryKoraExtension.java
@@ -1,6 +1,7 @@
 package ru.tinkoff.kora.database.annotation.processor.extension;
 
 import ru.tinkoff.kora.annotation.processor.common.CommonUtils;
+import ru.tinkoff.kora.annotation.processor.common.ProcessingErrorException;
 import ru.tinkoff.kora.database.annotation.processor.DbUtils;
 import ru.tinkoff.kora.kora.app.annotation.processor.extension.ExtensionResult;
 import ru.tinkoff.kora.kora.app.annotation.processor.extension.KoraExtension;
@@ -10,10 +11,12 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
+import java.util.Objects;
 
 public class RepositoryKoraExtension implements KoraExtension {
     private final Elements elements;
@@ -33,10 +36,45 @@ public class RepositoryKoraExtension implements KoraExtension {
         if (element.getKind() != ElementKind.INTERFACE && (element.getKind() != ElementKind.CLASS || !element.getModifiers().contains(Modifier.ABSTRACT))) {
             return null;
         }
-        if (CommonUtils.findDirectAnnotation(element, DbUtils.REPOSITORY_ANNOTATION) == null) {
-            return null;
+
+        final TypeElement typeElement;
+        if (CommonUtils.findDirectAnnotation(element, DbUtils.REPOSITORY_ANNOTATION) != null) {
+            typeElement = (TypeElement) this.types.asElement(typeMirror);
+        } else {
+            var candidates = roundEnvironment.getRootElements().stream()
+                .filter(candidate -> candidate instanceof TypeElement)
+                .map(candidate -> {
+                    if (types.isAssignable(candidate.asType(), typeMirror)
+                        && !types.isSameType(candidate.asType(), typeMirror)) {
+                        if (CommonUtils.findDirectAnnotation(candidate, DbUtils.REPOSITORY_ANNOTATION) != null) {
+                            return ((TypeElement) candidate);
+                        } else {
+                            return types.directSupertypes(candidate.asType()).stream()
+                                .filter(parentType -> parentType instanceof DeclaredType)
+                                .map(parentType -> ((DeclaredType) parentType))
+                                .filter(parentType -> CommonUtils.findDirectAnnotation(parentType.asElement(), DbUtils.REPOSITORY_ANNOTATION) != null)
+                                .findFirst()
+                                .map(parentType -> ((TypeElement) parentType.asElement()))
+                                .orElse(null);
+                        }
+                    } else {
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .toList();
+
+            if (candidates.isEmpty()) {
+                return null;
+            }
+
+            if (candidates.size() > 1) {
+                throw new ProcessingErrorException("Found '%s' suitable candidates for: %s".formatted(candidates.size(), element), element);
+            } else {
+                typeElement = candidates.get(0);
+            }
         }
-        var typeElement = (TypeElement) this.types.asElement(typeMirror);
+
         var packageElement = this.elements.getPackageOf(typeElement);
         var repositoryName = CommonUtils.getOuterClassesAsPrefix(typeElement) + typeElement.getSimpleName().toString() + "_Impl";
         var packageName = packageElement.getQualifiedName();
