2c7908f30bdd96a35ce82b18c7fc5129874ac70a
Anton Kurako <goodforgod.dev@gmail.com>
2023-07-18 15:31:09 +0300

    R2dbcExtensionTest reinforced

--- database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/r2dbc/R2dbcExtensionTest.java
+++ database/database-annotation-processor/src/test/java/ru/tinkoff/kora/database/common/annotation/processor/r2dbc/R2dbcExtensionTest.java
@@ -1,5 +1,6 @@
 package ru.tinkoff.kora.database.common.annotation.processor.r2dbc;
 
+import io.r2dbc.spi.Row;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Mono;
 import ru.tinkoff.kora.annotation.processor.common.TestUtils;
@@ -13,7 +14,11 @@ import ru.tinkoff.kora.database.r2dbc.mapper.result.R2dbcRowMapper;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
 public class R2dbcExtensionTest {
+
     @Test
     void testEntity() throws Exception {
         TestUtils.testKoraExtension(new TypeRef<?>[]{
@@ -26,5 +31,25 @@ public class R2dbcExtensionTest {
             TypeRef.of(R2dbcEntity.TestEntityFieldR2dbcResultColumnMapperNonFinal.class),
             TypeRef.of(R2dbcRowMapper.class, String.class)
         );
+    }
+
+    public record TestRow(String f1, String f2) {}
+
+    @Test
+    void testRowMapper() throws Exception {
+        var cl = TestUtils.testKoraExtension(new TypeRef<?>[]{
+                TypeRef.of(R2dbcRowMapper.class, TestRow.class),
+            }
+        );
+        var k = cl.loadClass("ru.tinkoff.kora.database.common.annotation.processor.r2dbc.$R2dbcExtensionTest_TestRowR2dbcRowMapper");
+        var mapper = (R2dbcRowMapper<TestRow>) k.getConstructors()[0].newInstance();
+        var row = mock(Row.class);
+
+        when(row.get("f1", String.class)).thenReturn("test1");
+        when(row.get("f2", String.class)).thenReturn("test2");
+        var o1 = mapper.apply(row);
+        assertThat(o1).isEqualTo(new TestRow("test1", "test2"));
+        verify(row).get("f1", String.class);
+        verify(row).get("f2", String.class);
     }
 }
